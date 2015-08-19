f58f43a1ee076d757f95a75ca40a784b02fa542b
GTL <cclark@gametheorylabs.com>
2015-08-19 15:24:56 -0500

    Added click and test tracking funcationality

--- battletest/js/main.js
+++ battletest/js/main.js
@@ -8,16 +8,18 @@ COption = function(txt, cat, value){
 	this.value = value;
 }
 
-Categories = {
-	explorer: 0,
-	achiever: 0,
-	socializer: 0,
-	killer: 0
-}
+
 
 BattleGame = {
+	Categories: {
+		explorer: 0,
+		achiever: 0,
+		socializer: 0,
+		killer: 0
+	},
 	Questions: [],
 	Answers: [],
+	totalAnswered: 0,
 	Init: function(){
 		BattleGame.LoadQuestions();
 		BattleGame.LoadHTML();
@@ -38,12 +40,12 @@ BattleGame = {
 			//Question 3
 		BattleGame.Answers[2] =[ new COption("Getting involved in the storyline", "socializer", 1),
 				  				 new COption("Getting the rewards at the end", "achiever", 1) ];
-		BattleGame.Questions[2] = new CQuestion("Which do you enjoy more in MUD quests", BattleGame.Answers[2]);	
+		BattleGame.Questions[2] = new CQuestion("Which do you enjoy more in MMO quests", BattleGame.Answers[2]);	
 
 			//Question 4
 		BattleGame.Answers[3] =[ new COption("Your equipment", "achiever", 1),
 				  				 new COption("Your personality", "socializer", 1) ];
-		BattleGame.Questions[3] = new CQuestion("Which would you rather be noticed for on a MUD", BattleGame.Answers[3]);	
+		BattleGame.Questions[3] = new CQuestion("Which would you rather be noticed for on a MMO", BattleGame.Answers[3]);	
 		
 			//Question 5
 		BattleGame.Answers[4] =[ new COption("Popular", "socializer", 1),
@@ -53,32 +55,32 @@ BattleGame = {
 			//Question 6
 		BattleGame.Answers[5] =[ new COption("Getting the latest gossip", "socializer", 1),
 				  				 new COption("Getting a new item", "achiever", 1) ];
-		BattleGame.Questions[5] = new CQuestion("Which do you enjoy more on a MUD", BattleGame.Answers[5]);	
+		BattleGame.Questions[5] = new CQuestion("Which do you enjoy more on a MMO", BattleGame.Answers[5]);	
 
 			//Question 7
 		BattleGame.Answers[6] =[ new COption("A private channel, over which you and your friends can communicate", "socializer", 1),
 				  				 new COption("Your own house, worth millions of gold coins", "achiever", 1) ];
-		BattleGame.Questions[6] = new CQuestion("Which would you rather have, as a player on a MUD", BattleGame.Answers[6]);	
+		BattleGame.Questions[6] = new CQuestion("Which would you rather have, as a player on a MMO", BattleGame.Answers[6]);	
 
 			//Question 8
 		BattleGame.Answers[7] =[ new COption("Running your own tavern", "socializer", 1),
 				  				 new COption("Making your own maps of the world, then selling them", "explorer", 1) ];
-		BattleGame.Questions[7] = new CQuestion("Which would you enjoy more as a MUD player", BattleGame.Answers[7]);	
+		BattleGame.Questions[7] = new CQuestion("Which would you enjoy more as a MMO player", BattleGame.Answers[7]);	
 
 			//Question 9
 		BattleGame.Answers[8] =[ new COption("The number of people", "socializer", 1),
 				  				 new COption("The number of areas to explore", "explorer", 1) ];
-		BattleGame.Questions[8] = new CQuestion("What's more important in a MUD to you", BattleGame.Answers[8]);	
+		BattleGame.Questions[8] = new CQuestion("What's more important in a MMO to you", BattleGame.Answers[8]);	
 
 			//Question 10
-		BattleGame.Answers[9] =[ new COption("The quality of roleplaying in a MUD", "socializer", 1),
+		BattleGame.Answers[9] =[ new COption("The quality of roleplaying in a MMO", "socializer", 1),
 				  				 new COption("The uniqueness of the features, and game mechanic", "explorer", 1) ];
 		BattleGame.Questions[9] = new CQuestion("What's more important to you", BattleGame.Answers[9]);	
 
 			//Question 11
 		BattleGame.Answers[10] =[ new COption("Ask a friend for help in killing it", "socializer", 1),
 				  				 new COption("Hide somewhere you know the monster won't follow", "explorer", 1) ];
-		BattleGame.Questions[10] = new CQuestion("You are being chased by a monster on a MUD, Do you", BattleGame.Answers[10]);	
+		BattleGame.Questions[10] = new CQuestion("You are being chased by a monster on a MMO, Do you", BattleGame.Answers[10]);	
 
 			//Question 12
 		BattleGame.Answers[11] =[ new COption("Get a big group of players to kill it.", "socializer", 1),
@@ -88,7 +90,7 @@ BattleGame = {
 			//Question 13
 		BattleGame.Answers[12] =[ new COption("A bard, who's a good friend of yours and who's great for entertaining you and your friends", "socializer", 1),
 				  				 new COption("A wizard, to identify the items that you find there", "explorer", 1) ];
-		BattleGame.Questions[12] = new CQuestion("You're a player on a mud, and about to go into an unknown dungeon. You have your choice of one more person for your party.", BattleGame.Answers[12]);	
+		BattleGame.Questions[12] = new CQuestion("You're a player on a MMO, and about to go into an unknown dungeon. You have your choice of one more person for your party.", BattleGame.Answers[12]);	
 
 			//Question 14
 		BattleGame.Answers[13] =[ new COption("Feared", "killer", 1),
@@ -128,12 +130,12 @@ BattleGame = {
 			//Question 21
 		BattleGame.Answers[20] =[ new COption("Exploring the new area, and finding out its history", "explorer", 1),
 				  				 new COption("Being the first to get the new equipment from the area", "achiever", 1) ];
-		BattleGame.Questions[20] = new CQuestion("On a MUD, a new area opens up. Which do you look forward to more?", BattleGame.Answers[20]);
+		BattleGame.Questions[20] = new CQuestion("On a MMO, a new area opens up. Which do you look forward to more?", BattleGame.Answers[20]);
 
 			//Question 22
 		BattleGame.Answers[21] =[ new COption("Someone who can run from any two points in the world, and really knows their way around.", "explorer", 1),
 				  				 new COption("The person with the best, most unique equipment in the game", "achiever", 1) ];
-		BattleGame.Questions[21] = new CQuestion("On a MUD, would you rather be known as:", BattleGame.Answers[21]);
+		BattleGame.Questions[21] = new CQuestion("On a MMO, would you rather be known as:", BattleGame.Answers[21]);
 
 			//Question 23
 		BattleGame.Answers[22] =[ new COption("Know more secrets than your friends?", "explorer", 1),
@@ -158,7 +160,7 @@ BattleGame = {
 			//Question 27
 		BattleGame.Answers[26] =[ new COption("Scholars", "explorer", 1),
 				  				 new COption("Assassins", "achiever", 1) ];
-		BattleGame.Questions[26] = new CQuestion("On a MUD, would rather join a clan of:", BattleGame.Answers[26]);
+		BattleGame.Questions[26] = new CQuestion("On a MMO, would rather join a clan of:", BattleGame.Answers[26]);
 
 			//Question 28
 		BattleGame.Answers[27] =[ new COption("A trivia contest", "explorer", 1),
@@ -173,7 +175,7 @@ BattleGame = {
 			//Question 30
 		BattleGame.Answers[29] =[ new COption("Knowledge", "explorer", 1),
 				  				 new COption("Power", "killer", 1) ];
-		BattleGame.Questions[29] = new CQuestion("On a MUD, would rather be known for", BattleGame.Answers[29]);
+		BattleGame.Questions[29] = new CQuestion("On a MMO, would rather be known for", BattleGame.Answers[29]);
 
 			//Question 31
 		BattleGame.Answers[30] =[ new COption("Explore a new area", "explorer", 1),
@@ -229,6 +231,13 @@ BattleGame = {
 	},
 	LoadHTML: function(){
 		BattleGame.AddHTMLQuestions();
+		BattleGame.AddEventListeners();
+	},
+	AddEventListeners: function(){
+		for(var i = 0; i < BattleGame.Questions.length; i++){
+			document.getElementById('q'+i+'a0').addEventListener('click', BattleGame.ProccessClick, false);
+			document.getElementById('q'+i+'a1').addEventListener('click', BattleGame.ProccessClick, false);
+		}
 	},
 	AddHTMLQuestions: function(){
 		html = "";
@@ -247,12 +256,12 @@ BattleGame = {
 							'</div>' +
 							'<div class="row">' +
 							  '<div class="col-md-12">' +
-							  	'<button type="button" class="btn btn-primary btn-lg">' + option1  + '</button>' +
+							  	'<button id="q'+ id +'a0" type="button" class="btn btn-primary btn-lg">' + option1  + '</button>' +
 							  '</div>' +
 							'</div>' +  
 							'<div class="row">' +
 							  '<div class="col-md-6">' +
-							  	'<button type="button" class="btn btn-primary btn-lg">' + option2 + '</button>' +
+							  	'<button id="q'+ id +'a1" type="button" class="btn btn-primary btn-lg">' + option2 + '</button>' +
 							  '</div>' +
 							'</div>' +
 							'</div>' +
@@ -260,6 +269,47 @@ BattleGame = {
 		}
 
 		document.getElementById('test').innerHTML = html;
+	},
+	ProccessClick: function(e){
+		var id = e.currentTarget.id;
+	
+		var questionID = id.substring(1, id.indexOf('a'))|0;
+		var answerID = id.substring(id.indexOf('a') + 1, id.length)|0;
+
+		var question = BattleGame.Questions[questionID];
+		BattleGame.Categories[question.options[answerID].category] += question.options[answerID].value;
+
+		document.getElementById(questionID).style.display = "none";
+
+		BattleGame.totalAnswered++;
+
+		if(BattleGame.totalAnswered > 39){
+			console.log("You are finished!");
+			var cat = 0;
+			var output = "";
+			if(BattleGame.Categories.explorer > cat){
+				cat =  BattleGame.Categories.explorer;
+				output = "You Are An EXPLORER!"
+			}
+			else if(BattleGame.Categories.achiever > cat){
+				cat = BattleGame.Categories.achiever;
+				output = "You Are An ACHIEVER!";
+
+			}
+			else if(BattleGame.Categories.killer > cat){
+				cat = BattleGame.Categories.killer;
+				output = "You Are A KILLER!";
+
+			}
+			else if(BattleGame.Categories.socializer > cat){
+				cat = BattleGame.Categories.socializer;
+				output = "You Are A SOCIALIZER!";
+
+			}
+
+			document.getElementById('test').innerHTML = "<h1>" + output + "</h1>";
+
+		}
 	}
 }
 
