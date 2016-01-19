80be7a057bca6ea48c8c4cf1b89949c7d09c4593
GTL <cclark@gametheorylabs.com>
2016-01-18 23:09:32 -0600

    Added Data Processing and chart plotting

--- lostcell/scripts/lostcell.js
+++ lostcell/scripts/lostcell.js
@@ -1,4 +1,4 @@
-var jos = os;
+var jos = os; //Hello World
 var LostCell = {
 	Init: function(){
 		os.debugbar.AnchorConsole();
@@ -30,7 +30,8 @@ var LostCell = {
 		droneStateDroneImageWrapper: null,
 		droneStateAtomImageWrapper: null,
 		dataStateDroneImageWrapper: null,
-		dataStateAtomImageWrapper: null
+		dataStateAtomImageWrapper: null,
+		loginButton: null
 	},
 	FSM: null,
 	States: {
@@ -966,6 +967,8 @@ var LostCell = {
 			
 			LostCell.HTML.loginButton = document.getElementById("loginButton");
 			
+						 
+			
 		},
 		Console: function(){
 			
@@ -1042,6 +1045,40 @@ var LostCell = {
 				LostCell.HTML.dataStateAtomImageWrapper.addEventListener("mousedown", function(){LostCell.FSM.AnimatedTransition("Data");}, false);
 				LostCell.HTML.dataStateDroneImageWrapper.addEventListener("touchend", function(){LostCell.FSM.AnimatedTransition("Drone");}, false);
 				LostCell.HTML.dataStateAtomImageWrapper.addEventListener("touchend", function(){LostCell.FSM.AnimatedTransition("Data");}, false);
+				
+				//g2 = new Dygraph( document.getElementById("graphdiv2"),
+				//					"temperatures.csv", // path to CSV file
+				//					{});          // options
+				//
+				
+					data = {
+					labels: ["January", "February", "March", "April", "May", "June", "July"],
+					datasets: [
+						{
+							label: "My First dataset",
+							fillColor: "rgba(220,220,220,0.2)",
+							strokeColor: "rgba(220,220,220,1)",
+							pointColor: "rgba(220,220,220,1)",
+							pointStrokeColor: "#fff",
+							pointHighlightFill: "#fff",
+							pointHighlightStroke: "rgba(220,220,220,1)",
+							data: [65, 59, 80, 81, 56, 55, 40]
+						},
+						{
+							label: "My Second dataset",
+							fillColor: "rgba(151,187,205,0.2)",
+							strokeColor: "rgba(151,187,205,1)",
+							pointColor: "rgba(151,187,205,1)",
+							pointStrokeColor: "#fff",
+							pointHighlightFill: "#fff",
+							pointHighlightStroke: "rgba(151,187,205,1)",
+							data: [28, 48, 40, 19, 86, 27, 90]
+						}
+					]
+				};
+				ctx = document.getElementById("myChart").getContext("2d");
+				options={};
+				myLineChart = new Chart(ctx).Line(data, options);	
 			}
 			LostCell.States.Data.Execute = function(obj, msg){
 				
@@ -1077,7 +1114,8 @@ var LostCell = {
 		//os.input.Register.Touch.Event.End(LostCell.FullScreen, LostCell.FullScreen);
 		//os.input.Register.Mouse.Event.Up(LostCell.FullScreen, LostCell.FullScreen);
 		
-		LostCell.FSM.Transition("Login");
+		//LostCell.FSM.Transition("Login");
+		LostCell.FSM.Transition("Data");
 	},
 	FullScreen: function(){
 		//LostCell.HTML.app.requestFullscreen();
